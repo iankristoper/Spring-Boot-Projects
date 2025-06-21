f28044f980fe742a9d331b609c10ee841e389d86
iankristoper <iankristoper26@gmail.com>
2025-06-22 01:51:18 +0800

    initial commit -> this is my first spring boot serious project. lol

---
+++ Online Course Enrollment System/src/main/java/dev/projects/onlinecoursesystem/OnlinecoursesystemApplication.java
@@ -0,0 +1,13 @@
+package dev.projects.onlinecoursesystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnlinecoursesystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OnlinecoursesystemApplication.class, args);
+	}
+
+}
---
+++ Online Course Enrollment System/src/test/java/dev/projects/onlinecoursesystem/OnlinecoursesystemApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.projects.onlinecoursesystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnlinecoursesystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
